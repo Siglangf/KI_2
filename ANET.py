fix numerical issue in anet and topp

ANET.py
@@ -114,13 +114,18 @@ class ANET(nn.Module):
         """
         state_tensor = torch.FloatTensor(state)
         D = self.get_action_probabilities(state_tensor)
-        if D[0] == np.nan:
+        if torch.isnan(D).any().item():
             # Fix numerical issue when renormalizing
-            legal_actions = [i for i in range(len(state[1:])) if state[i] == 0]
-            D = [0 for i in range(len(D))]
-            return D, random.choice(legal_actions)
-
+            D, action_index = self.get_random_move(state, D)
+            return D, action_index
         return D.tolist(), torch.argmax(D).item()
+    
+    def get_random_move(self, state, D):
+        state = state[1:]
+        legal_actions = [i for i in range(len(state)) if state[i] == 0]
+        D = [0 for i in range(len(D))]
+        action_index = random.choice(legal_actions)
+        return D, action_index
 
     def get_action_probabilities(self, state: torch.Tensor):
         """

TOPP.py
@@ -3,6 +3,7 @@ from Simworld import Hex
 import random
 import itertools
 import numpy as np
+import torch
 
 
 class TOPP:
@@ -44,11 +45,12 @@ class TOPP:
             D, action_index = self.agents[actors[player]].get_move(current_state)
             action = action_space[action_index]
             if not greedy:
-                print(D)
                 D /= np.sum(D)
-                action_index = np.random.choice(len(D), 1, p=D)[0]
-                action = action_space[action_index]
-
+                if np.isnan(D).any():
+                    D, action_index = self.agents[actors[player]].get_random_move(current_state, D)
+                else:
+                    action_index = np.random.choice(len(D), 1, p=D)[0]
+                    action = action_space[action_index]
             single_game.step(action)
             player = single_game.player
         winner = single_game.get_winner()